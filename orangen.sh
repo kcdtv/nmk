16d4cf864b40d221e9136576f2d0a315c1302a24
kcdtv <kcdtv78@gmail.com>
2017-09-09 16:48:11 +0200

    Update orangen.sh
    corrected checksum rules

--- orangen.sh
+++ orangen.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-version=1.2
+version=1.2.1
 # orangen.sh: Default pin generator for livebox 2.1 and livebox next
 # Algorithm and vulnerability by wifi-libre. For more details check https://www.wifi-libre.com/topic-869-todo-sobre-al-algoritmo-wps-livebox-arcadyan-orange-xxxx.html
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
@@ -38,7 +38,7 @@ D5=$(printf "%X\n" $(($(( 0x${serial:3:1} ^ 0x${wan:2:1}))%16)))
 D6=$(printf "%X\n" $(($(( 0x${serial:2:1} ^ 0x${wan:3:1}))%16)))
 D7=$(printf "%X\n" $(($(( 0x$K1 ^ 0x${serial:1:1}))%16)))
 pin=$(printf '%07d\n' $(($(printf '%d\n' 0x$D1$D2$D3$D4$D5$D6$D7)%10000000)))
-checksum=$((10 - $(($((${pin:0:1} * 3 + ${pin:1:1} + ${pin:2:1} * 3 + ${pin:3:1} + ${pin:4:1} * 3 + ${pin:5:1} + ${pin:6:1} * 3))%10))%10))
+checksum=$((((10 - $(($((${pin:0:1} * 3 + ${pin:1:1} + ${pin:2:1} * 3 + ${pin:3:1} + ${pin:4:1} * 3 + ${pin:5:1} + ${pin:6:1} * 3))%10))))%10))
 echo -e " $white     PIN por defecto: $orange $pin$checksum"
 echo -e "$grey
 Copyleft (C) 2017 kcdtv @ www.wifi-libre.com"
